b149b7bffb35d59f95335ba9dca94a5071a0f743
liguangbin <1234qwer@qq.com>
2020-09-23 17:00:45 +0800

    修改回调值

--- packages/local_auth/lib/local_auth.dart
+++ packages/local_auth/lib/local_auth.dart
@@ -75,7 +75,7 @@ class LocalAuthentication {
     AndroidAuthMessages androidAuthStrings = const AndroidAuthMessages(),
     IOSAuthMessages iOSAuthStrings = const IOSAuthMessages(),
     bool sensitiveTransaction = true,
-    ValueChanged<int> onPositiveCallback,
+    ValueChanged<String> onPositiveCallback,
   }) async {
     assert(localizedReason != null);
     final Map<String, dynamic> args = <String, dynamic>{
@@ -105,7 +105,7 @@ class LocalAuthentication {
             'authenticateWithBiometrics', args);
       } on PlatformException catch (e) {
         if (e.code == "-2000" || e.code == "-3000" || e.code == "-4000") {
-          onPositiveCallback(int.parse(e.code));
+          onPositiveCallback(e.code);
         } else {
           throw PlatformException(
               code: e.code, message: e.message, details: e.details);
